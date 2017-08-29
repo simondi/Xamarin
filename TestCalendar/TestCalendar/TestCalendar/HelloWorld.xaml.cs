3a7c44bdc385f3b4fdb685340edae5342b8e2f59
Simon Di <simondi@consumerprotectionbc.ca>
2017-08-29 13:50:32 -0700

    Initial Check in

---
+++ TestCalendar/TestCalendar/TestCalendar/HelloWorld.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TestCalendar
+{
+    public partial class HelloWorld : ContentPage
+    {
+        public HelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private async void SayHelloButton_OnClicked(object sender, EventArgs e)
+        {
+            var name = NameEntry.Text;
+            await DisplayAlert("Greeting", $"Hello {name}!", "Howdy");
+        }
+
+        private async void DoThisButton_OnClicked(object sender, EventArgs e)
+        {
+            var text = DoThis.Text;
+            await DisplayAlert("Greeting", $"Hello {text}!", "Howdy");
+        }
+    }
+}
